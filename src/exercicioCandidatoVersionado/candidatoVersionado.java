92c7c058f1041e88bbb48736fadd08f09c1ac6db
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-05-15 14:36:19 -0300

    Leitura dos outros dados

--- src/exercicioCandidatoVersionado/candidatoVersionado.java
+++ src/exercicioCandidatoVersionado/candidatoVersionado.java
@@ -9,16 +9,27 @@ public class candidatoVersionado {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		int escolaridade;
+		int escolaridade, expProfissional;
+		char disponibilidade, habilitacao;
+		
 		// Leitura da escolaridade
 		System.out.println("Qual sua escolaridade?");
 		System.out.println("1) Ensino Fundamental");
 		System.out.println("2) Ensino Médio");
-		System.out.println("3) Ensino Superior)");
+		System.out.println("3) Ensino Superior");
 		System.out.println("4) Pós-graduação");
 		System.out.print("Digite uma opção: ");
 		
 		escolaridade = sc.nextInt();
+		System.out.println();
+		// Leitura dos demais dados
+		
+		System.out.print("Você tem quantos anos de experiência profissional? ");
+		expProfissional = sc.nextInt();
+		System.out.print("Você tem disponibilidade para viajar (S/N)? ");
+		disponibilidade = sc.next().charAt(0);
+		System.out.print("Você tem habilitação de motorista (S/N)? ");
+		habilitacao = sc.next().charAt(0);
 		
 		sc.close();
 
